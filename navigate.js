ccb77727bd1124fcc9d8fe39363ae922e8070191
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-08-15 16:21:13 +0200

    js navigation on live server

---
+++ navigate.js
@@ -0,0 +1,37 @@
+function navigateTo(page,folders) {
+    let navLink = document.getElementById(page);
+    let siteURL = setSiteURL(folders);
+
+    switch(page){
+        case "home":
+        navLink.href = siteURL+"index.html";
+        break;
+        case "blog":
+        navLink.href = siteURL+ "Blogs.html";
+        break;
+        case "data-art":
+        navLink.href = siteURL+ "DataArt.html";
+        break;
+        case "data-vis":
+        navLink.href = siteURL+ "DAtaVis.html";
+        break;
+        case "design":
+        navLink.href = siteURL+ "Design.html";
+        break;
+    }
+}
+
+function setSiteURL(folders) {
+    let siteURLPrefix = "";
+    
+    if(folders==0){
+        return siteURLPrefix;
+    }
+
+    for (let index = 0; index < folders; index++) {
+       siteURLPrefix += "../";
+        
+    }
+
+    return siteURLPrefix;
+}
