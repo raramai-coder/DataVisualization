6a0c8b4e9af309d142c3e0f1b741c3ba54ab0208
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-11-20 13:13:29 +0200

    fix navigation

--- navigate.js
+++ navigate.js
@@ -38,3 +38,50 @@ function setSiteURL(folders) {
 
     return siteURLPrefix;
 }
+
+const pages = [
+	{ url: "../index.html", name: "Home", id: "home" },
+	{ url: "../Blogs.html", name: "Blog", id: "blog" },
+	{ url: "../Design.html", name: "Design", id: "design" },
+	{ url: "../DataArt.html", name: "Data Art", id: "data-art" },
+	{ url: "../DataVisualisations/DataVis.html", name: "Data Visualisation >", id: "data-vis" },
+];
+
+
+let navigationContainer = document.getElementById("nav-list");
+
+pages.forEach(element => {
+    const listItem = document.createElement("li");
+    const link = document.createElement("a");
+
+    link.innerText = element.name;
+
+    link.onclick= function() {
+        link.href = element.url;
+    }
+
+    link.id = element.id;
+   
+    if (element.id == "data-vis") {
+	    const button = document.createElement("div");
+	    button.id = "data-vis-button";
+        button.append(link);
+        listItem.append(button);
+    }else{
+        listItem.append(link);
+    }
+    
+    if (document.title == element.name) {
+		link.classList.add("active");
+
+        const line = document.createElement("div");
+        line.classList.add("underline");
+
+        listItem.append(line);
+	}
+
+    navigationContainer.append(listItem); 
+  
+});
+
+//console.log(document.title);
